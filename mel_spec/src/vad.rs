b838b8a15b746e220686d3e432c5ff1f953dc2ad
jbrough <jamie1612@gmail.com>
2024-08-25 15:23:45 +0100

    revert change to vad

--- mel_spec/src/vad.rs
+++ mel_spec/src/vad.rs
@@ -115,6 +115,33 @@ impl VoiceActivityDetector {
     }
 }
 
+fn vad_on(edge_info: &EdgeInfo, n: usize) -> bool {
+    let intersected_columns = &edge_info.intersected_columns;
+
+    if intersected_columns.is_empty() {
+        return false; // No intersected columns, so return false
+    }
+
+    let mut contiguous_count = 1; // Count of contiguous intersected columns
+    let mut prev_index = intersected_columns[0];
+
+    for &index in &intersected_columns[1..] {
+        if index == prev_index + 1 {
+            contiguous_count += 1;
+        } else {
+            contiguous_count = 1;
+        }
+
+        if contiguous_count >= n {
+            return true;
+        }
+
+        prev_index = index;
+    }
+
+    false // If no contiguous segment of n or more intersected columns is found, return false
+}
+
 /// Performs edge detection on the spectrogram using a fast Sobel operator
 fn vad_boundaries(frames: &[Array2<f64>], settings: &DetectionSettings) -> EdgeInfo {
     let array_views: Vec<_> = frames.iter().map(|a| a.view()).collect();
@@ -375,6 +402,7 @@ mod tests {
         let edge_info = vad_boundaries(&[frames.clone()], &settings);
 
         let elapsed = start.elapsed().as_millis();
+        dbg!(elapsed);
         let img = as_image(
             &[frames.clone()],
             &edge_info.non_intersected(),
@@ -397,12 +425,14 @@ mod tests {
         let start = std::time::Instant::now();
         let file_path = "../testdata/quantized_mel_golden.tga";
         let dequantized_mel = load_tga_8bit(file_path).unwrap();
+        dbg!(&dequantized_mel);
 
         let frames = to_array2(&dequantized_mel, n_mels);
 
         let edge_info = vad_boundaries(&[frames.clone()], &settings);
 
         let elapsed = start.elapsed().as_millis();
+        dbg!(elapsed);
         let img = as_image(
             &[frames.clone()],
             &edge_info.non_intersected(),
@@ -438,5 +468,6 @@ mod tests {
             if let Some(_) = stage.add(&mel) {}
         }
         let elapsed = start.elapsed().as_millis();
+        dbg!(elapsed);
     }
 }
